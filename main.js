839dd5871bdb1d9dea2cb3c0b485b30af8c461aa
Daniel <dotonesimple@gmail.com>
2023-02-06 18:28:20 +0100

    add form support

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const express = require('express')
 var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+
 const app = express()
 const port = 8080
 
 //cookie middleware
 app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     if(!req.cookies.user) {
         res.cookie("user", Math.floor(Math.random()*Number.MAX_SAFE_INTEGER));
@@ -17,6 +20,11 @@ app.use('/', express.static('public'));
 
 app.get('/test', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/form/', (req, res) => {
+  console.log(req.body)
+  res.send('Rezept angenommen! ' + req.body.text)
 })
 
 app.listen(port, () => {
