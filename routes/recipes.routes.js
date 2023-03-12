ca000e91e59991f58f182530091ade564b14f39a
Emma <emma-sophie.betz@web.de>
2023-03-12 15:04:15 +0100

    Add delete request to remove recipe from favorites. Add functionality to change button text.

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -48,8 +48,19 @@ module.exports = function(app, db) {
         }
     });
 
-    //need to access user id to send a favorites post request and a get favorites request to database
-    // to do: give user_id as param to post request, add get request, change button functionality based on whether recipe is already in favorites (change to un-favoritize), add filter function to categories page to show favorite recipes
+    app.delete('/remove_recipe_from_favorites', (req, res) => {
+        var sql = "DELETE FROM favorite WHERE fav_rec_id = ? AND fav_use_id = ?";
+        if (req.body.fav_req_id && req.body.fav_use_id) {
+            var params = [req.body.fav_use_id, req.body.fav_req_id];
+            db.run(sql, params, (err) => {
+                if (err) {
+                    res.status(400).json({"error":err.message});
+                    return;
+                }
+                //res.reload();
+            })
+        }
+    });
 
     app.post('/add_recipe', (req, res) => {
         var sql = "INSERT INTO recipes (rec_title, rec_ingredients, rec_needed_time, rec_instructions, rec_cat_id) VALUES (?,?,?,?,?)";
@@ -96,7 +107,7 @@ module.exports = function(app, db) {
                 res.status(400).send(`Es wurde kein Nutzer mit "${req.cookies.user}" gefunden.`);
                 return;
             }
-            let old_rows = rows;
+            let user_rows = rows;
             sql = "SELECT * FROM recipes WHERE rec_title = ?";
             params = [req.params.recipe];
             db.all(sql, params, (err, rows) => {
@@ -108,9 +119,25 @@ module.exports = function(app, db) {
                     res.status(400).send(`Es wurde kein Rezepte namens "${req.params.recipe}" gefunden.`);
                     return;
                 }
+                let recipe_rows = rows;
+
+                sql = "SELECT * FROM favorite WHERE rec_id = ?";
+                params = [req.params.recipe.rec_id];
+                var is_favorite = true; 
+                db.all(sql, params, (err, rows) => {
+                    if (err) {
+                        res.status(400).json({"error":err.message});
+                        return;
+                    }
+                    if (rows.length < 1) {
+                        is_favorite = false;
+                        return;
+                    }
+                })
                 res.render('pages/recipe', {
-                    recipe: rows[0],
-                    user: old_rows[0],
+                    recipe: recipe_rows[0],
+                    user: user_rows[0],
+                    favorite: is_favorite,
                 });
             });
         });
