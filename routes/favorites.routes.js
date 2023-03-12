40242783364f7debb8db5a162d32d902110d9b46
Emma <emma-sophie.betz@web.de>
2023-03-12 14:45:58 +0100

    Add favorites route and ejs page to show all recipes that have been added to favorites by user

---
+++ routes/favorites.routes.js
@@ -0,0 +1,36 @@
+module.exports = function(app, db) {
+    app.get('/favorites', (req, res) => {
+
+        var sql = "SELECT * FROM user WHERE use_session_cookie = ?";
+        var params = [req.cookies.user];
+        db.all(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({"error":err.message});
+                return;
+            }
+            if (rows.length < 1) {
+                res.status(400).send(`Es wurde kein Nutzer mit "${req.cookies.user}" gefunden.`);
+                return;
+            }
+            let old_rows = rows;
+
+            var sql = "SELECT * FROM recipes, favorite WHERE rec_id = fav_rec_id AND use_id = ? ORDER BY rec_title";
+            var params = [old_rows[0]];
+            db.all(sql, params, (err, rows) => {
+                if (err) {
+                    res.status(400).json({"error":err.message});
+                    return;
+                }
+                if (rows.length < 1) {
+                    res.status(400).send(`Es wurden keine favorisierten Rezepte gefunden.`);
+                    return;
+                }
+                res.render('pages/favorites', {
+                    recipes: rows
+                });
+            });
+        });
+
+        
+    });
+};
