b24b7764a283fcf4b7734a781b7458d552dd7c0d
Emma <emma-sophie.betz@web.de>
2023-03-15 16:41:03 +0100

    Add get-request for /index.html because it is a requirement from teacher

--- routes/landing_page.routes.js
+++ routes/landing_page.routes.js
@@ -13,4 +13,18 @@ module.exports = function(app, db) {
             });
         });
     });
+    app.get('/index.html', (req, res) => {
+        var sql = "SELECT * FROM categories ORDER BY cat_name";
+        var params = [];
+        db.all(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({"error":err.message});
+                return;
+            }
+            res.render('pages/landing_page', {
+                categories: rows
+            });
+        });
+    });
+
 }
