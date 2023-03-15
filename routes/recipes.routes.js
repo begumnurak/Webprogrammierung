f17231bd9fcc1eb3095941657a1bc57b37f0f264
Emma <emma-sophie.betz@web.de>
2023-03-15 16:06:16 +0100

    Change rec_image to rec_image_link

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -36,7 +36,7 @@ module.exports = function(app, db) {
     });
 
     app.post('/add_recipe', (req, res) => {
-        var sql = "INSERT INTO recipes (rec_title, rec_ingredients, rec_needed_time, rec_instructions, rec_image, rec_cat_id) VALUES (?,?,?,?,?,?)";
+        var sql = "INSERT INTO recipes (rec_title, rec_ingredients, rec_needed_time, rec_instructions, rec_image_link, rec_cat_id) VALUES (?,?,?,?,?,?)";
         if (req.body.title && req.body.needed_time && req.body.instructions && req.body.ingredients && req.body.category && req.body.image) {
             var params = [req.body.title, req.body.ingredients, req.body.needed_time, req.body.instructions, req.body.image, req.body.category];
             db.run(sql, params, (err) => {
@@ -52,7 +52,7 @@ module.exports = function(app, db) {
     });
 
     app.post('/add_recipe_json', (req, res) => {
-        var sql = "INSERT INTO recipes (rec_title, rec_ingredients, rec_needed_time, rec_instructions, rec_image, rec_cat_id) VALUES (?,?,?,?,?,?)";
+        var sql = "INSERT INTO recipes (rec_title, rec_ingredients, rec_needed_time, rec_instructions, rec_image_link, rec_cat_id) VALUES (?,?,?,?,?,?)";
         if (req.body.json && req.body.category) {
             let json = JSON.parse(req.body.json);
             var params = [json.title, json.ingredients, json.needed_time, json.instructions, json.image, req.body.category];
